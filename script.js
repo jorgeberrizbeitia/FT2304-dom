c296407fc27530d00541e95635f76e3446b35af1
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-04-18 16:07:20 +0200

    done

--- script.js
+++ script.js
@@ -95,13 +95,12 @@ let finalThoughtDOM = document.querySelector("#final-thoughts");
 
 console.log(finalThoughtDOM.id);
 
-
 setTimeout(() => {
   finalThoughtDOM.id = "new-id";
   // los elementos de DOM/HTML solo deberian tener un id
-}, 3000)
+}, 3000);
 
-console.log(finalThoughtDOM.className)
+console.log(finalThoughtDOM.className);
 
 // finalThoughtDOM.className = "new-class";
 // esto no será lo correcto, reemplazamos todas las clases.
@@ -110,66 +109,189 @@ console.log(finalThoughtDOM.className)
 // tenemos formas de agregar, remover y verificar las clases del elemento
 // classList
 
-finalThoughtDOM.classList.add("new-class") // añadir clase
-finalThoughtDOM.classList.remove("best-practice") // remueve clase
+finalThoughtDOM.classList.add("new-class"); // añadir clase
+finalThoughtDOM.classList.remove("best-practice"); // remueve clase
 
-console.log( finalThoughtDOM.classList.contains("after-footer") )
+console.log(finalThoughtDOM.classList.contains("after-footer"));
 // devuelve un booleano, si la clase está incluida o no
 
 // Investigar classList.toggle()
 
 setTimeout(() => {
-  finalThoughtDOM.classList.add("bold")
-}, 3000)
-
+  finalThoughtDOM.classList.add("bold");
+}, 3000);
 
 //* Crear y eliminar elementos de DOM
 
-let emptyContainerDOM = document.querySelector("#empty-container")
-console.log(emptyContainerDOM)
+let emptyContainerDOM = document.querySelector("#empty-container");
+console.log(emptyContainerDOM);
 
 // *crear un elemento
-let newElementDOM = document.createElement("h4") 
+let newElementDOM = document.createElement("h4");
 // crea una referencia de un elemento de DOM
 
-newElementDOM.innerText = "Primer elemento creado desde JS"
+newElementDOM.innerText = "Primer elemento creado desde JS";
 
-console.log(newElementDOM)
+console.log(newElementDOM);
 //* Añadir un elemento a la visualización
-emptyContainerDOM.append(newElementDOM)
+emptyContainerDOM.append(newElementDOM);
 
-let newElementDOM2 = document.createElement("h4") 
-newElementDOM2.innerText = "Segundo elemento creado desde JS"
+let newElementDOM2 = document.createElement("h4");
+newElementDOM2.innerText = "Segundo elemento creado desde JS";
 
-emptyContainerDOM.append(newElementDOM2)
-
+emptyContainerDOM.append(newElementDOM2);
 
 //* remover => .remove()
 setTimeout(() => {
-  newElementDOM.remove() // quitarlo de la visualización
+  newElementDOM.remove(); // quitarlo de la visualización
   // document.body.remove()
-}, 3000)
+}, 3000);
 
 setTimeout(() => {
-  newElementDOM2.remove()
-  emptyContainerDOM.append(newElementDOM)
-  emptyContainerDOM.append(newElementDOM2)
-}, 6000)
-
-
+  newElementDOM2.remove();
+  emptyContainerDOM.append(newElementDOM);
+  emptyContainerDOM.append(newElementDOM2);
+}, 6000);
 
 // innerText vs innerHTML
-let footerDOM = document.querySelector("#footer")
+let footerDOM = document.querySelector("#footer");
 
-console.log(footerDOM)
-console.log(footerDOM.innerText)
+console.log(footerDOM);
+console.log(footerDOM.innerText);
 // innerText es SOLO el contendio texto (string) dentro de las etiquetas que abren y cierran
 
-console.log(footerDOM.innerHTML)
+console.log(footerDOM.innerHTML);
 // innerHTML es una version stringificada de la estructura que exista dentro de las etiquetas que abren y cierran
-let newStr = "patata"
-footerDOM.innerHTML = `<h1 id="last-title"> <span> ${newStr} </span> </h1>`
+let newStr = "patata";
+footerDOM.innerHTML = `<h1 id="last-title"> <span> ${newStr} </span> </h1>`;
 
-console.log(footerDOM.innerHTML)
+console.log(footerDOM.innerHTML);
 
+//* EVENTS
+
+// addEventListener => escuchar cuando ocurra ese evento evento especifica, y nos permite ejecutar algo adicional.
+
+// let eventTitleDOM = document.querySelector("#event-title");
+
+// // Ejemplo de almacenar los estilos originales para revertir los cambios.
+// // let starterStyles = eventTitleDOM.style
+
+// eventTitleDOM.addEventListener("mouseenter", () => {
+//   eventTitleDOM.innerText = "PATATA!";
+//   // puedo signar estilos directamente desde JS
+//   eventTitleDOM.style.color = "magenta";
+//   eventTitleDOM.style.backgroundColor = "green";
+//   // cuando trabajamos con estilos en DOM, todos los nombres de propiedades de CSS, se escriben con camelcase
+// });
+
+// eventTitleDOM.addEventListener("mouseleave", () => {
+//   eventTitleDOM.innerText = "EVENTS!";
+//   eventTitleDOM.style.color = "black";
+//   eventTitleDOM.style.backgroundColor = "white";
+//   // eventTitleDOM.style = starterStyles
+// });
+
+// eventos más comunes:
+// mouseenter
+// mouseleave
+// click
+// dblclick
+// keydown
+// keyup
+// submit
+// focus
+
+// Reestructuración de codigo
+
+// FUNCIONES
+
+function hoverTitle() {
+  eventTitleDOM.innerText = "PATATA!";
+  // puedo signar estilos directamente desde JS
+  eventTitleDOM.style.color = "magenta";
+  eventTitleDOM.style.backgroundColor = "green";
+  // cuando trabajamos con estilos en DOM, todos los nombres de
+}
+
+function unHoverTitle() {
+  eventTitleDOM.innerText = "EVENTS!";
+  eventTitleDOM.style.color = "black";
+  eventTitleDOM.style.backgroundColor = "white";
+}
+
+function sumDom() {
+  console.log("probando");
+  // counterDOM.innerText = Number(counterDOM.innerText) + 1
+  counterDOM.innerText++;
+  // let counter = 0;
+  // counter++;
+}
+
+function restDom() {
+  counterDOM.innerText--;
+}
+
+function addToList() {
+  console.log("probando añadir elemento");
+
+  // buscar el valor de texto a agregar
+  let inputDOM = document.querySelector("#name");
+  // console.log(inputDOM.innerText) // no funciona, input es etiqueta de autocierre
+  let newText = inputDOM.value;
+
+  // crear un elemento de lista (li)
+  let liDOM = document.createElement("li");
+
+  // añadir el texto al li
+  liDOM.innerText = newText;
+  console.log(liDOM)
+
+  // añadir el li a la lista
+  let ulDOM = document.querySelector("#output-list")
+  ulDOM.append(liDOM)
+
+
+  // ejemplo con innerHTML
+  // let ulDOM = document.querySelector("#output-list")
+  // ulDOM.innerHTML += `<li>${newText}</li>`
+
+
+}
+
+function deleteBtn(event) {
+  console.log("probando borrar boton")
+  // console.log(event) // event es TODA la informacion del evento que está ocurriendo.
+  console.log(event.target) // el elemento de DOM sobre el cual está ocurriendo el evento
+
+  // buscar el button especifo que estoy clickando
+
+  // y luego hacer .remove()
+  // event.target.remove()
+
+  // suponemos que el elemento que quiero borrar no es un solo boton, sino que son todos los botones.
+
+  // podriamos buscar el div por selector id o class o...
+
+  console.log(event.target.parentNode) // el elemento de DOM padre donde ocurre el evento
+  event.target.parentNode.remove()
+
+}
+
+// ADDEVENTLISTENERS
+let eventTitleDOM = document.querySelector("#event-title");
+let counterDOM = document.querySelector("#counter span");
+let increaseBtn = document.querySelector("#increment");
+let decreaseBtn = document.querySelector("#decrement");
+let addItemBtn = document.querySelector("#add-btn");
+
+eventTitleDOM.addEventListener("mouseenter", hoverTitle);
+eventTitleDOM.addEventListener("mouseleave", unHoverTitle);
+increaseBtn.addEventListener("click", sumDom);
+decreaseBtn.addEventListener("click", restDom);
+addItemBtn.addEventListener("click", addToList);
+
+let allBtns = document.querySelectorAll(".last-btn")
+allBtns.forEach((eachBtnDOM) => {
+  eachBtnDOM.addEventListener("click", deleteBtn)
+})
 
